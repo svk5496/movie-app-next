b76cae376598cd224183ec73ee81d36406e0caa7
svk5496 <otter0513@naver.com>
2023-03-30 22:00:55 +0900

    chore: health goal

---
+++ pages/healthGoal.tsx
@@ -0,0 +1,11 @@
+import Seo from "@/components/atoms/Seo";
+import Header from "@/components/molecules/Header";
+
+export default function HealthGoal() {
+  return (
+    <div>
+      <Seo title="영양소"></Seo>
+      영양소
+    </div>
+  );
+}
